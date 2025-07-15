171218b5b84a77272a9fbd7e8834c6cf28f312d3
mrafath123 <md.rafath08@gmail.com>
2025-07-15 09:27:01 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.devdatic.devpush',
+  appName: 'pushnotify',
+  webDir: 'www'
+};
+
+export default config;
